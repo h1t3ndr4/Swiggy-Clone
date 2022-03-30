0dab9868e58bc4ad3d70301d50a6147be907f7c9
tusharsahu2599 <tksahu1234@gmail.com>
2022-03-30 09:25:36 +0530

    footer completed

---
+++ swiggy/src/App.jsx
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Footer from './components/footer/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <h3>this page is working conditions</h3>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
+
+
