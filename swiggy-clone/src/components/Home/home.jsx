0989efc780ff6398973b2462e8d2e810e3b27e98
Premkumar-hulikoppe <premuhulikoppe@gmail.com>
2022-03-30 12:17:16 +0530

    Added NavBar and Carousels

--- swiggy-clone/src/components/Home/home.jsx
+++ swiggy-clone/src/components/Home/home.jsx
@@ -1,11 +1,13 @@
 
 import { NavBar } from "./Navbar/Navbar";
+import { ControlledCarousel } from "./Carasouel/imgSlider";
 
 export const Home = () => {
 
     return (
         <> 
            <NavBar />
+           <ControlledCarousel />
         </>
     )
 }
