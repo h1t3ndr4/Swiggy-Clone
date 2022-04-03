3eb2d8b98902337bdcc9f132fa277271b8e41839
h1t3ndr4 <hitumali10@gmail.com>
2022-04-03 17:49:35 +0530

    search page added

--- swiggy-clone/src/App.jsx
+++ swiggy-clone/src/App.jsx
@@ -6,6 +6,7 @@ import { Routes, Route } from "react-router-dom";
 import React from "react";
 import Cart from "./components/cart/Cart";
 import PaymentOffers from "../src/components/Offers/PaymentOffers";
+import Search from "./components/Search/Search";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path="/help" element={<Help />}></Route>
         <Route path="/my-account/:comp" element={<MyAccountsOrders />} />
         <Route path="/cart" element={<Cart />}></Route>
+        <Route path="/search" element={<Search />}></Route>
 
         <Route path="/offers/payment" element={<PaymentOffers />}></Route>
       </Routes>
